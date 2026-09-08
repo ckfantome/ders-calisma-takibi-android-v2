a182ea03e167b800e4a795dd471c6a085570e924
Adem <ckfantome@gmail.com>
2026-09-08 07:48:05 +0300

    Bump version to 0.50.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         // bagimliliklar - Compose/CameraX/OpenCV - zaten API 21'i destekliyor).
         minSdk = 24
         targetSdk = 34
-        versionCode = 52
-        versionName = "0.50.0"
+        versionCode = 53
+        versionName = "0.50.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
